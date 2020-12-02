46a19103ddca3fa2b516a4e2c2a4d7428e9157bf
Juliette Pretot <julsh@google.com>
2020-12-02 11:45:54 +0000

    wip

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'none'; sandbox allow-scripts;"
-  )
-  return next()
+app.get('/', (req, res) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self';")
+  res.setHeader('Content-Security-Policy', "default-src 'self';")
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
+app.get('/foo.html', (req, res) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'none'; script-src 'unsafe-inline'; style-src 'unsafe-inline';"
+  )
+  res.setHeader('Cache-Control', 'public, max-age=604800, immutable')
+  res.sendFile(path.join(__dirname + '/foo.html'))
 })
 
 app.listen(3000)
